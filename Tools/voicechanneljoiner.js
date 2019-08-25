f1fcabbabc5c021e425a9e09d5c8069abb365529
Julian <33635418+xjulianCodes@users.noreply.github.com>
2019-08-26 02:29:38 +0300

    اصلاح مشاكل
    مو مجرب الاداة ؟

--- Tools/voicechanneljoiner.js
+++ Tools/voicechanneljoiner.js
@@ -1,18 +1,19 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
 const fs = require("fs");
 const cache = JSON.parse(fs.readFileSync("./caches.json", "utf8"))
 const config = JSON.parse(fs.readFileSync("./tokens.json", "utf8"))
 const token = config.tokens;
-client.on("ready", () => {
+
+token.forEach(acc => {
+    const client = new Discord.Client();
+    
+    client.on("ready", () => {
     setInterval(() => {
         client.guilds.get(cache.serverid).channels.get(cache.vchid).join().then(() => {
             client.guilds.get(cache.serverid).voiceConnection.disconnect();
+            });
     }, cache.delay);
-
-    })
 })
-
-token.forEach(acc => {
+    
     client.login(acc)
     });
